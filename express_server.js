93ad91c2b8dde54899e3b2b84d4eb40f7c24bd48
Nima <nima.mohamed@hotmail.com>
2023-02-20 19:57:31 -0800

    W3 D1

--- express_server.js
+++ express_server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
