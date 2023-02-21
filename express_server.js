a051260af6ec1655d937d9d3003361aaa31683a3
Nima <nima.mohamed@hotmail.com>
2023-02-20 20:10:46 -0800

    W3 D1

--- express_server.js
+++ express_server.js
@@ -8,6 +8,10 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
